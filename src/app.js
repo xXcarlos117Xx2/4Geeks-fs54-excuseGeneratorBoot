6b263d268796eee3ed107db66d174cc349eeb9a5
Carlos Atanes Vences <carlos117g@gmail.com>
2023-12-07 22:01:34 +0000

    Agregado bootstrap y simplificado JS

--- src/app.js
+++ src/app.js
@@ -1,42 +1,44 @@
 /* eslint-disable */
-// Aqui teneis mi Magnus Opus
-// El codigo esta hecho complicado a proposito. Mas abajo teneis la version simplificada
-// Si quereis lo repito sin "bromas", pero siempre viene bien reirse un rato...
 import "bootstrap";
 import "./style.css";
+
 window.onload = function() {
+  ///////////////////////////////////////////////////////////////////////////////
+  //                                                                           //
+  //                                 Funciones                                 //
+  //                                                                           //
+  ///////////////////////////////////////////////////////////////////////////////
+
+  //Funcion para escoger elemento aleatorio dentro del array
+
+  const elementoAleatorio = arr => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
+
+  ///////////////////////////////////////////////////////////////////////////////
+  //  Objeto con los "array" dentro, cada uno con los valores de las excusas.  //
+  //                                                                           //
+  //                     objeto {etiqueta : valor}                             //
+  ///////////////////////////////////////////////////////////////////////////////
+
   const excuses = {
     who: ["The dog", "My grandma", "His turtle", "My bird"],
     action: ["ate", "peed", "crushed", "broke"],
     what: ["my homework", "the keys", "the car"],
-    when: [
-      "before the class",
-      "right on time",
-      "when I finished",
-      "during my lunch",
-      "while I was praying"
-    ]
+    when: ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"]
   };
-  let result = "";
-  for (let i = 0; i < Object.keys(excuses).length; i++) {
-    result +=
-      excuses[Object.keys(excuses)[i]][
-        Math.floor(Math.random() * excuses[Object.keys(excuses)[i]].length)
-      ];
-    if (i < Object.keys(excuses).length - 1) {
-      result += " ";
-    }
-  }
-  document.getElementById("excuse").innerHTML = result;
+
+  ///////////////////////////////////////////////////////////////////////////////
+  //                                                                           //
+  //                                 Programa                                  //
+  //                                                                           //
+  ///////////////////////////////////////////////////////////////////////////////
+
+  //1. Pedimos al documento el id "excuse" y le editamos el HTML interior ==> "document.getElementById("excuse").innerHTML"
+  //Usamos la función "Object.values()" para que nos devuelva el objeto como un array. ==> Object.values(excuses)
+  //Usamos el ".map" para escoger un elemento de cada uno junto con la funcion de "elementoAleatorio". ==> .map(elementoAleatorio)
+  //Usamos ".join" para que junte todos los valores con espacios entre cada uno. ==> .join(" ")
+  //Todo esto se manda al HTML al elemento "excuse" que ya ha sido previamente formateado con Bootstrap.
+
+  document.getElementById("excuse").innerHTML = Object.values(excuses).map(elementoAleatorio).join(" ");
 };
-/*
-O puedes utilizar esto...
-
-Version simplificada:
-  const getRandomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
-    const result = Object.values(excuses)
-      .map(getRandomElement)
-      .join(" ");
-    document.getElementById("excuse").innerHTML = result;
-
-*/
